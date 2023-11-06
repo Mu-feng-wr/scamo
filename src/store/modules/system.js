0e5384bd729b1a413ece4f6cb1443375b1d84a9a
liangwt <320453942@qq.com>
2023-11-06 14:44:07 +0800

    修改

--- src/store/modules/system.js
+++ src/store/modules/system.js
@@ -1,6 +1,4 @@
 
-import { login, getMenu } from '@/api/dup/system.js'
-import Cookies from 'js-cookie'
 const getDefaultState = () => {
   return {
     dupToken: '',
@@ -11,28 +9,16 @@ const getDefaultState = () => {
 const state = getDefaultState()
 const mutations = {
   SET_DUP_TOKEN: (state, token) => {
-    state.dupToken = token
   },
   SET_MENULIST: (state, menulist) => {
-    state.menuList = menulist
   }
 }
 const actions = {
   loginHandler({ commit }, formData) {
-    return new Promise((resolve, reject) => {
-      login(formData).then(res => {
-        Cookies.set('dupToken', res.token)
-        getMenu().then(res => {
-          commit('SET_MENULIST', res.menuList)
-        })
-        resolve(res)
-      })
-    })
+
   },
   getMenulist({ commit }) {
-    getMenu().then(res => {
-      commit('SET_MENULIST', res.menuList)
-    })
+
   }
 }
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,69 +1,13 @@
 <template>
-  <el-container class="user-layout">
-    <div class="login-left" :style="backgroundStyle" />
-    <el-container class="user-layout login-right">
-      <el-main>
-        <signin />
-      </el-main>
-      <el-footer height="auto">
-        <copyright />
-      </el-footer>
-    </el-container>
-  </el-container>
+  <div>22222</div>
 </template>
+
 <script>
-import bgImg1 from '@/assets/images/imgs/banner_1.png'
-import bgImg2 from '@/assets/images/imgs/banner_2.png'
-import bgImg3 from '@/assets/images/imgs/banner_3.png'
-import bgImg4 from '@/assets/images/imgs/banner_4.png'
-import signin from './components/signin.vue'
-import copyright from '@/components/Copyright/index.vue'
 export default {
-  components: {
-    signin,
-    copyright
-  },
-  data() {
-    return {
-      backgroundImageUrls: [bgImg1, bgImg2, bgImg3, bgImg4]
-    }
-  },
-  computed: {
-    currentBackgroundIndex() {
-      return localStorage.getItem('currentBackgroundIndex') || 0
-    },
-    backgroundStyle() {
-      return {
-        'background-image': `url(${this.backgroundImageUrls[this.currentBackgroundIndex]})`
-      }
-    }
-  }
+
 }
 </script>
-<style lang="scss" scoped>
-.user-layout {
-  height: 100%;
-  background-color: #fff;
-  .el-main {
-    padding: 0;
-    height: inherit;
-  }
-  .el-header {
-    padding: 0;
-  }
-  .el-footer {
-    padding: 8px 0;
-  }
-}
-.login-left {
-	width: 60%;
-	position: relative;
-	background-size: 100% 100%;
-	background-repeat: no-repeat;
-	.logo{
-		position: absolute;
-		left: 80px;
-		top: 40px;
-	}
-}
+
+<style>
+
 </style>
