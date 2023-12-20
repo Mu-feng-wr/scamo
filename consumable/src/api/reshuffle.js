cfafee0f322cd42d2657b636e418e856b1bc30f2
liangwt <320453942@qq.com>
2023-12-20 16:09:11 +0800

    提交代码

---
+++ consumable/src/api/reshuffle.js
@@ -0,0 +1,44 @@
+import request from '@/utils/request'
+
+// 查询耗材异动单信息列表
+export function listReshuffle(query) {
+  return request({
+    url: '/consume/reshuffle/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询耗材异动单信息详细
+export function getReshuffle(consumableReshuffleId) {
+  return request({
+    url: '/consume/reshuffle/' + consumableReshuffleId,
+    method: 'get'
+  })
+}
+
+// 新增耗材异动单信息
+export function addReshuffle(data) {
+  return request({
+    url: '/consume/reshuffle',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改耗材异动单信息
+export function updateReshuffle(data) {
+  return request({
+    url: '/consume/reshuffle',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除耗材异动单信息
+export function delReshuffle(consumableReshuffleId) {
+  return request({
+    url: '/consume/reshuffle/' + consumableReshuffleId,
+    method: 'delete'
+  })
+}
